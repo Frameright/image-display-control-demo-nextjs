4db50192c48ca4168aa9307acf933bce4144076d
Ilkka Järstä <jarsta@gmail.com>
2023-08-23 11:55:27 +0200

    Update page title

--- components/meta.tsx
+++ components/meta.tsx
@@ -34,7 +34,7 @@ const Meta = () => {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`A statically generated demo using the Image Display Control React Component and Next.js.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
